fa27a1fee430c9dd2c8d09dbf8a4a3ca59b81206
Island-Fever <handoseok@gmail.com>
2023-08-07 22:34:39 +0900

    chore: Spring Project 기본 설정

---
+++ server/backend-server/src/main/java/com/teamtwentyone/BackendServerApplication.java
@@ -0,0 +1,13 @@
+package com.teamtwentyone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendServerApplication.class, args);
+	}
+
+}
