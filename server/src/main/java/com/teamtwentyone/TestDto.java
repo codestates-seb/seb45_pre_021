23154ad8d997bc1cadbf64596744d73659832ba8
Island-Fever <handoseok@gmail.com>
2023-08-09 00:58:34 +0900

    ✨feat: ngrok 보여주기용

--- server/src/main/java/com/teamtwentyone/TestDto.java
+++ server/src/main/java/com/teamtwentyone/TestDto.java
@@ -1,4 +1,16 @@
 package com.teamtwentyone;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TestDto {
+    private Long id;
+    private String title;
+    private String content;
 }
