f8d10175e9ded2ab73e1aa81defe2ef547846dcf
Jiwon Lee <giwon.yi339@gmail.com>
2023-08-10 15:16:32 +0900

    ✨ feat: 레이아웃 추가

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,26 @@
-import { styled } from 'styled-components';
+import styled from 'styled-components';
 
 const Sidebar = () => {
-  return <aside>Sidebar</aside>;
+  return (
+    <aside>
+      <div>
+        <h2>Home</h2>
+
+        <h2>Public</h2>
+        <ul>
+          <li>Questions</li>
+          <li>Tags</li>
+          <li>Users</li>
+          <li>Companies</li>
+        </ul>
+        <h2>Collectives</h2>
+        <h2>Explore Collectives</h2>
+        <h2>Teams</h2>
+        <button>Create Free Team</button>
+        <p>Looking for your Teams?</p>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
