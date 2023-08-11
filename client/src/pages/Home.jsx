bb951c23a7dfe1e2fb6d11917e749ac9d179fe4b
Jiwon Lee <giwon.yi339@gmail.com>
2023-08-11 12:16:03 +0900

    ✨ feat: 메인페이지 마크업

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,28 @@
-const Home = () => {};
+import Footer from './../components/Footer.jsx';
+
+const Home = () => {
+  return (
+    <>
+      <header>Header</header>
+      <body>
+        <div>
+          <h1>Welcome to Stack Overflow</h1>
+        </div>
+        <div>
+          <div>
+            <div>Frontend</div>
+            <div>Backend</div>
+          </div>
+          <div>
+            <h2>Skills</h2>
+            <div>Frontend Skills</div>
+            <div>Backend skills</div>
+          </div>
+        </div>
+      </body>
+      <Footer />
+    </>
+  );
+};
 
 export default Home;
