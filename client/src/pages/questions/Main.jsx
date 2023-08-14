82ea46233d7ba1b2ccff1cae4279237286e8563a
Jiwon Lee <giwon.yi339@gmail.com>
2023-08-14 17:28:36 +0900

    ✨ feat: questions 페이지 생성 및 마크업

--- client/src/pages/questions/Main.jsx
+++ client/src/pages/questions/Main.jsx
@@ -1,3 +1,35 @@
-const Main = () => {};
+import { styled } from 'styled-components';
+
+const Main = () => {
+  return (
+    <QuestionsLayout>
+      <div>
+        <div>
+          <p>Top questions</p>
+        </div>
+        <div>
+          <button>Ask Question</button>
+          <span>Newest</span>
+          <span>Unanswered</span>
+        </div>
+      </div>
+      <QuestionBox>
+        <div>0 answers</div>
+        <div>Title</div>
+        <div>Title Info</div>
+      </QuestionBox>
+    </QuestionsLayout>
+  );
+};
 
 export default Main;
+
+const QuestionsLayout = styled.section`
+  margin-top: 56px;
+  height: 100vh;
+  width: 80%;
+`;
+
+const QuestionBox = styled.div`
+  width: 80%;
+`;
