97f979821c79b862e4ae389ae31195ff5f6f12b5
Jiwon Lee <giwon.yi339@gmail.com>
2023-08-16 00:20:38 +0900

    💄 design: 화면 우측 article 마크업 및 css 추가

--- client/src/pages/questions/Main.jsx
+++ client/src/pages/questions/Main.jsx
@@ -5,19 +5,46 @@ const Main = () => {
     <QuestionsLayout>
       <div>
         <div>
-          <p>Top questions</p>
+          <div>
+            <p>All questions</p>
+            <button>Ask Question</button>
+          </div>
+          <div>
+            <span>0 questions</span>
+            <span>Newest</span>
+            <span>Unanswered</span>
+          </div>
         </div>
         <div>
-          <button>Ask Question</button>
-          <span>Newest</span>
-          <span>Unanswered</span>
+          <div>0 answers</div>
+          <div>Title</div>
+          <div>Title Info</div>
         </div>
       </div>
-      <QuestionBox>
-        <div>0 answers</div>
-        <div>Title</div>
-        <div>Title Info</div>
-      </QuestionBox>
+      <QuickLinkLayout>
+        <h3>The Overflow Blog</h3>
+        <div>
+          <p>Why everyone should be an AppSec specialist (Ep. 598)</p>
+          <p>Want better answers from your data? Ask better questions</p>
+        </div>
+        <h3>Featured on Meta</h3>
+        <div>
+          <p>Moderation strike: results of negotations</p>
+          <p>
+            Our Design Vision for Stack Overflow and the Stack Exchange network
+          </p>
+          <p>Temporary policy: Generative AI (e.g., ChatGPT) is banned</p>
+          <p>Preview of Search and Question-Asking Powered by GenAI</p>
+          <p>Collections: A New Feature for Collectives on Stack Overflow</p>
+        </div>
+        <h3>Hot Meta Posts</h3>
+        <div>
+          <p>
+            What is the edit policy today for questions about obsolete
+            functionality?
+          </p>
+        </div>
+      </QuickLinkLayout>
     </QuestionsLayout>
   );
 };
@@ -25,11 +52,35 @@ const Main = () => {
 export default Main;
 
 const QuestionsLayout = styled.section`
-  margin-top: 56px;
   height: 100vh;
+
+  margin: 56px auto 0 auto;
+
+  background-color: beige;
   width: 80%;
+
+  display: flex;
+  /* align-items: center; */
+  justify-content: center;
 `;
 
-const QuestionBox = styled.div`
-  width: 80%;
+const QuickLinkLayout = styled.article`
+  display: flex;
+  flex-direction: column;
+
+  width: 300px;
+
+  background-color: #fdf7e2;
+
+  h3 {
+    /* background-color: #fff; */
+    background-color: #fbf3b5;
+    height: 40px;
+    font-size: 15px;
+    color: rgba(0, 0, 0, 0.7);
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    color: 1px solid rgba(0, 0, 0, 0.8);
+  }
 `;
