1190b23bfec2eccc3ba738cd13a63232df8e6ba7
Jiwon Lee <giwon.yi339@gmail.com>
2023-08-16 17:51:32 +0900

    ✨ feat: Filter 및 Ignore 박스 추가

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -2,36 +2,55 @@ import { styled } from 'styled-components';
 
 const RightSidebar = () => {
   return (
-    <RightAside>
-      <h3>The Overflow Blog</h3>
-      <QuickLinkBox>
-        <p>Why everyone should be an AppSec specialist (Ep. 598)</p>
-        <p>Want better answers from your data? Ask better questions</p>
-      </QuickLinkBox>
-      <h3>Featured on Meta</h3>
-      <QuickLinkBox>
-        <p>Moderation strike: results of negotations</p>
-        <p>
-          Our Design Vision for Stack Overflow and the Stack Exchange network
-        </p>
-        <p>Temporary policy: Generative AI (e.g., ChatGPT) is banned</p>
-        <p>Preview of Search and Question-Asking Powered by GenAI</p>
-        <p>Collections: A New Feature for Collectives on Stack Overflow</p>
-      </QuickLinkBox>
-      <h3>Hot Meta Posts</h3>
-      <QuickLinkBox>
-        <p>
-          What is the edit policy today for questions about obsolete
-          functionality?
-        </p>
-      </QuickLinkBox>
-    </RightAside>
+    <RightSide>
+      <OverflowContainer>
+        <h3>The Overflow Blog</h3>
+        <QuickLinkBox>
+          <p>Why everyone should be an AppSec specialist (Ep. 598)</p>
+          <p>Want better answers from your data? Ask better questions</p>
+        </QuickLinkBox>
+        <h3>Featured on Meta</h3>
+        <QuickLinkBox>
+          <p>Moderation strike: results of negotations</p>
+          <p>
+            Our Design Vision for Stack Overflow and the Stack Exchange network
+          </p>
+          <p>Temporary policy: Generative AI (e.g., ChatGPT) is banned</p>
+          <p>Preview of Search and Question-Asking Powered by GenAI</p>
+          <p>Collections: A New Feature for Collectives on Stack Overflow</p>
+        </QuickLinkBox>
+        <h3>Hot Meta Posts</h3>
+        <QuickLinkBox>
+          <p>
+            What is the edit policy today for questions about obsolete
+            functionality?
+          </p>
+        </QuickLinkBox>
+      </OverflowContainer>
+      <CustomFilterContainer>
+        <button>Custom Filters</button>
+        <FilterInfo>
+          <span>Create a custom filter</span>
+        </FilterInfo>
+      </CustomFilterContainer>
+      <CustomFilterContainer>
+        <button>Ignored Tags </button>
+        <FilterInfo>
+          <span>Add an ignored tab </span>
+        </FilterInfo>
+      </CustomFilterContainer>
+    </RightSide>
   );
 };
 
 export default RightSidebar;
 
-const RightAside = styled.article`
+const RightSide = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+const OverflowContainer = styled.article`
   display: flex;
   flex-direction: column;
 
@@ -64,4 +83,27 @@ const QuickLinkBox = styled.div`
       cursor: pointer;
     }
   }
+`;
+
+const CustomFilterContainer = styled.article`
+  width: 300px;
+  height: 95px;
+  margin: 20px 0;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 6px;
+
+  button {
+    height: 45px;
+    width: 100%;
+    text-align: left;
+    font-size: 14px;
+    padding: 16px;
+  }
+`;
+
+const FilterInfo = styled.div`
+  height: 50px;
+  color: #0074cc;
+  padding: 16px;
+  font-size: 13px;
 `;
