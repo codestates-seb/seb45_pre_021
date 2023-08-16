3a846fc24f2c0f7a46cf81b50522993eafd94752
Jiwon Lee <giwon.yi339@gmail.com>
2023-08-16 11:17:38 +0900

    ✨ feat: 오른쪽 사이드바 컴포넌트 분리

---
+++ client/src/components/RightSidebar.jsx
@@ -0,0 +1,63 @@
+import { styled } from 'styled-components';
+
+const RightSidebar = () => {
+  return (
+    <RightAside>
+      <h3>The Overflow Blog</h3>
+      <QuickLinkBox>
+        <p>Why everyone should be an AppSec specialist (Ep. 598)</p>
+        <p>Want better answers from your data? Ask better questions</p>
+      </QuickLinkBox>
+      <h3>Featured on Meta</h3>
+      <QuickLinkBox>
+        <p>Moderation strike: results of negotations</p>
+        <p>
+          Our Design Vision for Stack Overflow and the Stack Exchange network
+        </p>
+        <p>Temporary policy: Generative AI (e.g., ChatGPT) is banned</p>
+        <p>Preview of Search and Question-Asking Powered by GenAI</p>
+        <p>Collections: A New Feature for Collectives on Stack Overflow</p>
+      </QuickLinkBox>
+      <h3>Hot Meta Posts</h3>
+      <QuickLinkBox>
+        <p>
+          What is the edit policy today for questions about obsolete
+          functionality?
+        </p>
+      </QuickLinkBox>
+    </RightAside>
+  );
+};
+
+export default RightSidebar;
+
+const RightAside = styled.article`
+  display: flex;
+  flex-direction: column;
+
+  width: 300px;
+  height: 530px;
+
+  background-color: #fdf7e2;
+
+  h3 {
+    /* background-color: #fff; */
+    background-color: #fbf3b5;
+    height: 40px;
+    font-size: 15px;
+    color: rgba(0, 0, 0, 0.7);
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    color: 1px solid rgba(0, 0, 0, 0.8);
+  }
+`;
+
+const QuickLinkBox = styled.div`
+  padding: 14px;
+
+  p {
+    font-size: 14px;
+    margin-bottom: 10px;
+  }
+`;
--- client/src/pages/questions/Main.jsx
+++ client/src/pages/questions/Main.jsx
@@ -1,4 +1,5 @@
 import { styled } from 'styled-components';
+import RightSidebar from '../../components/RightSidebar.jsx';
 
 const Main = () => {
   return (
@@ -21,30 +22,7 @@ const Main = () => {
           <div>Title Info</div>
         </div>
       </div>
-      <QuickLinkLayout>
-        <h3>The Overflow Blog</h3>
-        <QuickLinkBox>
-          <p>Why everyone should be an AppSec specialist (Ep. 598)</p>
-          <p>Want better answers from your data? Ask better questions</p>
-        </QuickLinkBox>
-        <h3>Featured on Meta</h3>
-        <QuickLinkBox>
-          <p>Moderation strike: results of negotations</p>
-          <p>
-            Our Design Vision for Stack Overflow and the Stack Exchange network
-          </p>
-          <p>Temporary policy: Generative AI (e.g., ChatGPT) is banned</p>
-          <p>Preview of Search and Question-Asking Powered by GenAI</p>
-          <p>Collections: A New Feature for Collectives on Stack Overflow</p>
-        </QuickLinkBox>
-        <h3>Hot Meta Posts</h3>
-        <QuickLinkBox>
-          <p>
-            What is the edit policy today for questions about obsolete
-            functionality?
-          </p>
-        </QuickLinkBox>
-      </QuickLinkLayout>
+      <RightSidebar />
     </QuestionsLayout>
   );
 };
@@ -62,35 +40,4 @@ const QuestionsLayout = styled.section`
   display: flex;
   /* align-items: center; */
   justify-content: center;
-`;
-
-const QuickLinkLayout = styled.article`
-  display: flex;
-  flex-direction: column;
-
-  width: 300px;
-  height: 530px;
-
-  background-color: #fdf7e2;
-
-  h3 {
-    /* background-color: #fff; */
-    background-color: #fbf3b5;
-    height: 40px;
-    font-size: 15px;
-    color: rgba(0, 0, 0, 0.7);
-    display: flex;
-    align-items: center;
-    padding: 10px;
-    color: 1px solid rgba(0, 0, 0, 0.8);
-  }
-`;
-
-const QuickLinkBox = styled.div`
-  padding: 14px;
-
-  p {
-    font-size: 14px;
-    margin-bottom: 10px;
-  }
 `;
