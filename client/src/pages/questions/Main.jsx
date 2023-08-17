0d2d3161065f397593dbe3720ef06ffaeb236cf0
Jiwon Lee <giwon.yi339@gmail.com>
2023-08-17 11:02:10 +0900

    💄 design: 버튼 css styling

--- client/src/pages/questions/Main.jsx
+++ client/src/pages/questions/Main.jsx
@@ -82,7 +82,7 @@ const Main = () => {
       <QuestionContainer>
         <Header>
           <p>All Questions</p>
-          <button>Ask Question</button>
+          <Button>Ask Question</Button>
         </Header>
         <SubHeader>
           <p>{post.length} Questions</p>
@@ -133,7 +133,8 @@ const QuestionPage = styled.section`
   display: flex;
   /* align-items: center; */
   justify-content: center;
-  width: 90%;
+  /* width: 90%; */
+  /* width: 100vw; */
   padding: 16px;
 
   margin: 0 auto;
@@ -141,7 +142,7 @@ const QuestionPage = styled.section`
 `;
 
 const QuestionContainer = styled.article`
-  width: 50vw;
+  width: 55vw;
 `;
 
 const Header = styled.div`
@@ -225,157 +226,20 @@ const UserInfo = styled.div`
   justify-content: flex-end;
 `;
 
-//   return (
-//     <ContainerTest>
-//       <QuestionsLayout>
-//         <QuestionsContainer>
-//           <QuestionsHeader>
-//             <h2>All questions</h2>
-//             <button>Ask Question</button>
-//           </QuestionsHeader>
-//           <FilterLayout>
-//             <span>0 questions</span>
-//             <div>
-//               <button>Newest</button>
-//               <button>Unanswered</button>
-//             </div>
-//           </FilterLayout>
-//           {questions.map((box) => {
-//             return (
-//               <QuestionBox key={box.id}>
-//                 <QuestionStats>
-//                   <StatInfo>
-//                     <span>0 votes</span>
-//                   </StatInfo>
-//                   <StatInfo>
-//                     <span>0 answers</span>
-//                   </StatInfo>
-//                   <StatInfo>
-//                     <span>0 views</span>
-//                   </StatInfo>
-//                 </QuestionStats>
-//                 <QuestionContent>
-//                   <h3>{box.title}</h3>
-//                   <p>{box.desc}</p>
-//                   <ContentInfo>
-//                     <button>{box.tags}</button>
-//                     <div>
-//                       <span>{box.name}</span>
-//                       <span>asked 29 secs ago</span>
-//                     </div>
-//                   </ContentInfo>
-//                 </QuestionContent>
-//               </QuestionBox>
-//             );
-//           })}
-//         </QuestionsContainer>
-//       </QuestionsLayout>
-//       <RightSidebar />
-//     </ContainerTest>
-//   );
-// };
-// const ContainerTest = styled.section
-//   display: flex;
-//   /* align-items: center; */
-//   justify-content: center;
-//   margin-top: 56px;
-// `;
-
-// const QuestionsLayout = styled.article`
-//   height: 100vh;
-
-//   /* margin: 56px auto 0 auto; */
-//   /* width: 100vw; */
-//   /* background-color: beige; */
-
-//   display: flex;
-//   justify-content: center;
-// `;
-
-// const QuestionsContainer = styled.div`
-//   /* width: 596px; */
-//   width: 40vw;
-
-//   display: flex;
-//   flex-direction: column;
-
-//   padding: 10px;
-// `;
-
-// const QuestionsHeader = styled.div`
-//   width: 100%;
-//   height: 50px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-// `;
-
-// const FilterLayout = styled.div`
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   margin-bottom: 10px;
-
-//   button {
-//     padding: 10px;
-//     border: 0.5px solid rgba(0, 0, 0, 0.2);
-//     border-radius: 4px;
-//   }
-// `;
-
-// const QuestionBox = styled.div`
-//   height: 150px;
-//   /* border-top: 0.5px solid gray; */
-//   border-bottom: 0.5px solid rgba(0, 0, 0, 0.2);
-//   /* width: 50vw; */
-//   margin: 0 auto;
-//   display: flex;
-//   padding: 10px;
-//   align-items: center;
-//   /* justify-content: space-between; */
-// `;
-
-// const QuestionStats = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: flex-start;
-//   width: 150px;
-
-//   font-size: 14px;
-
-//   /* font-size: 14px; */
-// `;
-
-// const StatInfo = styled.div`
-//   font-size: 14px;
-
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-
-//   span {
-//     margin: 5px 0;
-//   }
-// `;
-
-// const QuestionContent = styled.div`
-//   h3 {
-//     color: #0074cc;
-//     width: 465px;
-//   }
-
-//   p {
-//     font-size: 13px;
-//     margin: 5px 0;
-//   }
-
-//   display: flex;
-//   flex-direction: column;
-// `;
-
-// const ContentInfo = styled.div`
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   font-size: 13px;
-// `;
+const Button = styled.button`
+  width: fit-content;
+  height: 40px;
+  padding: 10px;
+  background-color: #0a95ff;
+  color: #fff;
+  border: 1px solid none;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #0074cc;
+  }
+  &.disabled {
+    background-color: #84caff;
+    pointer-events: none;
+  }
+`;
