0670ec39a2cf7c23e62a83c9613f4cdb17514029
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-08-21 18:10:12 +0900

    🐛 fix: axios 404에러 해결

--- client/src/pages/users/Register.jsx
+++ client/src/pages/users/Register.jsx
@@ -21,12 +21,13 @@ const Register = () => {
 
   const { isLoggedIn } = useContext(LoginContext);
 
+  const nav = useNavigate();
+
   if (isLoggedIn) {
     nav('/questions');
   }
 
   const [clicked, setClicked] = useState(false);
-  const nav = useNavigate();
 
   const handleCaptchaChange = () => {
     setClicked((prevClicked) => !prevClicked);
