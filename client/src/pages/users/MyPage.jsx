f989bec274c4d9252b9cd0c935243b1828a34b34
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-08-22 23:43:56 +0900

    🐛 fix: 마이페이지에서 새로고침 시, 에러 발생 수정

--- client/src/pages/users/MyPage.jsx
+++ client/src/pages/users/MyPage.jsx
@@ -31,46 +31,52 @@ const MyPage = () => {
               )}
             </div>
             <UserInfo>
-              <h2>{userData.nickName}</h2>
-              <div className="userinfo--details">
-                <div className="userinfo--box">
-                  <svg
-                    aria-hidden="true"
-                    className="svg-icon iconCake"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                  >
-                    <path d="M9 4.5a1.5 1.5 0 0 0 1.28-2.27L9 0 7.72 2.23c-.14.22-.22.48-.22.77 0 .83.68 1.5 1.5 1.5Zm3.45 7.5-.8-.81-.81.8c-.98.98-2.69.98-3.67 0l-.8-.8-.82.8c-.49.49-1.14.76-1.83.76-.55 0-1.3-.17-1.72-.46V15c0 1.1.9 2 2 2h10a2 2 0 0 0 2-2v-2.7c-.42.28-1.17.45-1.72.45-.69 0-1.34-.27-1.83-.76Zm1.3-5H10V5H8v2H4.25C3 7 2 8 2 9.25v.9c0 .81.91 1.47 1.72 1.47.39 0 .77-.14 1.03-.42l1.61-1.6 1.6 1.6a1.5 1.5 0 0 0 2.08 0l1.6-1.6 1.6 1.6c.28.28.64.43 1.03.43.81 0 1.73-.67 1.73-1.48v-.9C16 8.01 15 7 13.75 7Z"></path>
-                  </svg>
-                  <span>Member for 10 days</span>
-                </div>
-                <div className="userinfo--box">
-                  <svg
-                    aria-hidden="true"
-                    className="svg-icon iconClock"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                  >
-                    <path d="M9 17c-4.36 0-8-3.64-8-8 0-4.36 3.64-8 8-8 4.36 0 8 3.64 8 8 0 4.36-3.64 8-8 8Zm0-2c3.27 0 6-2.73 6-6s-2.73-6-6-6-6 2.73-6 6 2.73 6 6 6ZM8 5h1.01L9 9.36l3.22 2.1-.6.93L8 10V5Z"></path>
-                  </svg>
-                  <span>Last seen this week</span>
-                </div>
+              {userData ? (
+                <>
+                  <h2>{userData.nickName}</h2>
+                  <div className="userinfo--details">
+                    <div className="userinfo--box">
+                      <svg
+                        aria-hidden="true"
+                        className="svg-icon iconCake"
+                        width="18"
+                        height="18"
+                        viewBox="0 0 18 18"
+                      >
+                        <path d="M9 4.5a1.5 1.5 0 0 0 1.28-2.27L9 0 7.72 2.23c-.14.22-.22.48-.22.77 0 .83.68 1.5 1.5 1.5Zm3.45 7.5-.8-.81-.81.8c-.98.98-2.69.98-3.67 0l-.8-.8-.82.8c-.49.49-1.14.76-1.83.76-.55 0-1.3-.17-1.72-.46V15c0 1.1.9 2 2 2h10a2 2 0 0 0 2-2v-2.7c-.42.28-1.17.45-1.72.45-.69 0-1.34-.27-1.83-.76Zm1.3-5H10V5H8v2H4.25C3 7 2 8 2 9.25v.9c0 .81.91 1.47 1.72 1.47.39 0 .77-.14 1.03-.42l1.61-1.6 1.6 1.6a1.5 1.5 0 0 0 2.08 0l1.6-1.6 1.6 1.6c.28.28.64.43 1.03.43.81 0 1.73-.67 1.73-1.48v-.9C16 8.01 15 7 13.75 7Z"></path>
+                      </svg>
+                      <span>Member for 10 days</span>
+                    </div>
+                    <div className="userinfo--box">
+                      <svg
+                        aria-hidden="true"
+                        className="svg-icon iconClock"
+                        width="18"
+                        height="18"
+                        viewBox="0 0 18 18"
+                      >
+                        <path d="M9 17c-4.36 0-8-3.64-8-8 0-4.36 3.64-8 8-8 4.36 0 8 3.64 8 8 0 4.36-3.64 8-8 8Zm0-2c3.27 0 6-2.73 6-6s-2.73-6-6-6-6 2.73-6 6 2.73 6 6 6ZM8 5h1.01L9 9.36l3.22 2.1-.6.93L8 10V5Z"></path>
+                      </svg>
+                      <span>Last seen this week</span>
+                    </div>
 
-                <div className="userinfo--box">
-                  <svg
-                    aria-hidden="true"
-                    className="svg-icon iconCalendar"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                  >
-                    <path d="M14 2h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h1V0h2v2h6V0h2v2ZM3 6v9h12V6H3Zm2 2h2v2H5V8Zm0 3h2v2H5v-2Zm3 0h2v2H8v-2Zm3 0h2v2h-2v-2Zm0-3h2v2h-2V8ZM8 8h2v2H8V8Z"></path>
-                  </svg>
-                  <span>Visited 2 days, 1 consecutive</span>
-                </div>
-              </div>
+                    <div className="userinfo--box">
+                      <svg
+                        aria-hidden="true"
+                        className="svg-icon iconCalendar"
+                        width="18"
+                        height="18"
+                        viewBox="0 0 18 18"
+                      >
+                        <path d="M14 2h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h1V0h2v2h6V0h2v2ZM3 6v9h12V6H3Zm2 2h2v2H5V8Zm0 3h2v2H5v-2Zm3 0h2v2H8v-2Zm3 0h2v2h-2v-2Zm0-3h2v2h-2V8ZM8 8h2v2H8V8Z"></path>
+                      </svg>
+                      <span>Visited 2 days, 1 consecutive</span>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                ''
+              )}
             </UserInfo>
           </HeaderLeft>
           <HeaderRight>
